fc08c5095c8aff95eb0bc94a08640b0058512de0
Giã Dương Đức Minh <giaduongducminh@gmail.com>
2026-01-06 19:14:03 +0700

    feat(client): implement ClientUseCase with CRUD operations for clients

--- internal/usecases/usecases.go
+++ internal/usecases/usecases.go
@@ -1,15 +1,1 @@
 package usecases
-
-import (
-	"context"
-
-	"github.com/ducminhgd/plan-craft/internal/entities"
-)
-
-type ClientRepository interface {
-	Create(ctx context.Context, client *entities.Client) (*entities.Client, error)
-	GetOne(ctx context.Context, id uint) (*entities.Client, error)
-	GetMany(ctx context.Context, qParams *entities.ClientQueryParams) ([]*entities.Client, int64, error)
-	Update(ctx context.Context, client *entities.Client) (int64, error)
-	Delete(ctx context.Context, id uint) error
-}
